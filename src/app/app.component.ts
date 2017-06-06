262c4108c1a864fc9cf01d0765e8239c1a317b55
Lucas Federico Torroba Hennigen <lucasfth@gmail.com>
2017-06-06 16:54:05 -0300

    feat(app): add basic test code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, NgModule, AfterViewChecked } from '@angular/core'
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements AfterViewChecked {
+  private datasets = [
+    {
+      label: "# of Votes",
+      data: [12, 19, 3, 5, 2, 3]
+    },
+    {
+      label: "# of Votes",
+      data: [19, 3, 5, 2, 3,33]
+    }
+  ];
+
+  private labels = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'];
+
+  private options = {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  };
+
+  ngAfterViewChecked() {
+    let timer = Observable.timer(2000, 1000);
+
+    /*timer.subscribe(() => {
+      this.datasets = [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3]
+      }];
+    });*/
+
+  }
 }
